0c0a8de30b0a2a38893627abb3a5dbe0702c060f
Marcos Luz <marcos.antonioluz36@gmail.com>
2026-03-20 20:33:45 -0300

    Cria a estrutura para imprimir o relatorio

--- report_script.tcl
+++ report_script.tcl
@@ -4,3 +4,11 @@ set file_data [read $fp]
 puts $file_data
 
 close $fp
+
+set T1 5; # Teste para validar as os prints;
+
+puts "=== RELATÓRIO DE CÉLULAS ===";
+puts "AND2: ${T1} instâncias";
+puts "XOR2: ${T1} instâncias";
+puts "flipflop_D: ${T1} instâncias";
+puts "TOTAL: ${T1} instâncias";
