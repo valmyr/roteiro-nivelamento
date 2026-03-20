a0bb1ce54e28d9a8ab6c78b549ecccc10598eb71
valmyr <valmir.silva@ee.ufcg.edu.br>
2026-03-20 20:03:48 -0300

    Adicionando hello.tcl

---
+++ hello.tcl
@@ -0,0 +1,8 @@
+puts "Olá, Mundo - Entre aspas"; #Este é um comentário após o commando
+# Este é um commentário no início de uma linha
+puts {Olá, Mundo - Entre chaves};
+puts {Exemplo de síntaxe de commentário inválido};  # *Erro* - não há ponto e vírgula!
+puts "Esta é a linha 1"; puts "esta é a linha 2";
+puts "Olá, Mundo; - Com um ponto e vŕigula dentro das aspas";
+#Palavras não precisam ser colocadas entre aspas, a menos que contenham espaços"
+puts OláMundo;
