982d4a77eb67df46fe0b0a51ef03bbbd3cb4167d
valmyr <valmir.silva@ee.ufcg.edu.br>
2026-03-20 20:53:06 -0300

    Contagem de instâncias de células AND e XOR

--- report_script.tcl
+++ report_script.tcl
@@ -1,14 +1,15 @@
 set fp [open "netlist.v" r]; # Diretório do arquivo 
 set file_txt [read $fp]; # Leitura do Arquivo netlist
 
-puts $file_txt
+#puts $file_txt
 
 close $fp
 
-set T1 5; # Teste para validar as os prints;
+set and [regexp -all {AND2} $file_txt]
+set xor [regexp -all {XOR2} $file_txt]
 
 puts "=== RELATÓRIO DE CÉLULAS ===";
-puts "AND2: ${T1} instâncias";
-puts "XOR2: ${T1} instâncias";
+puts "AND2: ${and} instâncias";
+puts "XOR2: ${xor} instâncias";
 puts "flipflop_D: ${T1} instâncias";
 puts "TOTAL: ${T1} instâncias";
