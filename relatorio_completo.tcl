be41ed316eecee4e2ef3342fc4071abd3bf2de64
Marcos Luz <marcos.antonioluz36@gmail.com>
2026-03-27 18:14:14 -0300

    Relatório completo da netlist

---
+++ relatorio_completo.tcl
@@ -0,0 +1,7 @@
+puts "============================"
+puts "--- RELATÓRIO COMPLETO ---"
+puts "============================\n"
+
+# Chamada para executar os outros relatórios
+source "report_script.tcl"
+#source "estatisticas_conexoes.tcl"
