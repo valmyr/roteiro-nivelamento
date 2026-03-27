dd15b0d182659ef24e876cfa48e1c057f01181ce
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2026-03-27 22:39:09 +0000

    criando pasta roteiro_shell/projeto

--- roteiro_shell/file_gen.sh
+++ roteiro_shell/file_gen.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/bash
 
+if [-d "./projeto/" ]; then
+    echo "A pasta projeto ja existe"
+else
+    mkdir projeto
 
-touch alu.v
-touch regfile.v
-touch top.v
-touch mux.v
-touch alut_tb.v
-touch defs.vh
-touch script.tcl
-touch sim.do
-touch README.md
+fi
+
+touch projeto/alu.v
+touch projeto/regfile.v
+touch projeto/top.v
+touch projeto/mux.v
+touch projeto/alut_tb.v
+touch projeto/defs.vh
+touch projeto/script.tcl
+touch projeto/sim.do
+touch projeto/README.md
