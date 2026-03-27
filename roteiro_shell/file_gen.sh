c418f2d610770b03014a825dab5029819f6eb561
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2026-03-27 22:42:41 +0000

    Ajuste do script file_gen.sh

--- roteiro_shell/file_gen.sh
+++ roteiro_shell/file_gen.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/bash
 
-if [-d "./projeto/" ]; then
+if [ -d "projeto/" ]; then
     echo "A pasta projeto ja existe"
 else
     mkdir projeto
-
+    touch projeto/alu.v
+    touch projeto/regfile.v
+    touch projeto/top.v
+    touch projeto/mux.v
+    touch projeto/alut_tb.v
+    touch projeto/defs.vh
+    touch projeto/script.tcl
+    touch projeto/sim.do
+    touch projeto/README.md
 fi
-
-touch projeto/alu.v
-touch projeto/regfile.v
-touch projeto/top.v
-touch projeto/mux.v
-touch projeto/alut_tb.v
-touch projeto/defs.vh
-touch projeto/script.tcl
-touch projeto/sim.do
-touch projeto/README.md
